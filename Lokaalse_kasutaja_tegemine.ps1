11d84b48c87c99545548ad9ad2fd708e551ae1c3
MargusB1 <margus.burk@voco.ee>
2022-11-14 09:50:27 +0200

    Lokaalse_kasutaja_tegemine.ps1

---
+++ Lokaalse_kasutaja_tegemine.ps1
@@ -0,0 +1,10 @@
+﻿# kasutaja andmed käsurea parameetrid
+param(
+$Kasutajanimi,
+$TaisNimi,
+$KontoKirjeldus
+)
+# loome kasutajale parool
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+# lisame kasutaja vastavate andmetega
+New-LocalUser "$Kasutajanimi" -Password $KasutajaParool -FullName "$Taisnimi" -Description "$Kontokirjeldus"
