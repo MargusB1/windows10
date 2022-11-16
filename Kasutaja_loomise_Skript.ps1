463cafcb2970688fc65a2e71558cfb486af84f2f
MargusB1 <margus.burk@voco.ee>
2022-11-16 11:37:58 +0200

    Kasutaja_loomise_Skript.ps1

---
+++ Kasutaja_loomise_Skript.ps1
@@ -0,0 +1,10 @@
+﻿#defineerime muutujad argumentide salvestamiseks
+param(
+$Kasutajanimi,
+$Taisnimi,
+$Kontokirjeldus
+)
+#loome parooli
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+#loome kasutaja parameetrid
+New-LocalUser $Kasutajanimi -Password $KasutajaParool -FullName $Taisnimi -Description $Kontokirjeldus
