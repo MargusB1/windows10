19dbc21d8fb069c9cf9b564d49e64273abe407bb
MargusB1 <margus.burk@voco.ee>
2022-11-16 11:40:04 +0200

    Kasutaja_kustutamine.ps1

---
+++ Kasutaja_kustutamine.ps1
@@ -0,0 +1,12 @@
+﻿$Eesnimi = Read-Host -Prompt "Sisesta oma eesnimi"
+$Perenimi = Read-Host -Prompt "Sisesta oma Perenimi"
+$Kasutajanimi = $Eesnimi + "." + $Perenimi
+$Taisnimi = $Eesnimi + " " + $Perenimi
+try{
+    Remove-LocalUser $Kasutajanimi -ErrorAction Stop
+}
+catch{
+    echo "Midagi läks valesti"
+    Exit
+}
+echo "Kasutaja on kustutatud"
